38699d95d57d302c17e77892b448b243e2069782
Brent McConnell <brmcconn@microsoft.com>
2019-10-25 11:56:38 -0400

    Update RedisConfiguration.java

--- src/main/java/net/guides/springboot/todomanagement/config/RedisConfiguration.java
+++ src/main/java/net/guides/springboot/todomanagement/config/RedisConfiguration.java
@@ -1,15 +1,6 @@
 package net.guides.springboot.todomanagement.config;
 
-import java.net.URI;
-import java.net.URISyntaxException;
-import java.util.Arrays;
-
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.cache.CacheManager;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
-import org.springframework.context.annotation.Profile;
 import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
